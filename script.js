d4eb8febdb1e1e9843b505a5757a81793f535dcd
cube-king <jacksonthomaspitts@gmail.com>
2025-06-19 12:30:28 -0400

    better visuals, almost finished with landing page

---
+++ script.js
@@ -0,0 +1,16 @@
+$("#crossplatform").hover(
+    function () { 
+        $("#features").css("background-image", "url('https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Test.svg/2560px-Test.svg.png')");
+    }
+);
+$("#playlist").hover(
+    function () { 
+        $("#features").css("background-image", "url('https://testimages.org/img/testimages_screenshot.jpg')");
+    }
+)
+$("#opensource").hover(
+    function () { 
+        $("#features").css("background-image", "url('https://a.fsdn.com/con/app/proj/testimages/screenshots/patterns_mosaic_a.png/max/max/1')");
+    }
+)
+
