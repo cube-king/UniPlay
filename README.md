374315c14b1c418c856ebb917280c84c597ee745
cube-king <jacksonthomaspitts@gmail.com>
2025-06-21 16:11:09 -0400

    player.js test changes and overall FAQ

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # UniPlay - a unification of your music streaming software!
 
-UniPlay, simply put, is a hub that unifies your music streaming services. Management of songs throughout platforms such as Soundcloud, Youtube and even Spotify! It allows for playlist merging, playing of 
+UniPlay, simply put, is a hub that unifies your music streaming services. Management of songs throughout platforms such as Soundcloud, Youtube and even Spotify! It allows for playlist merging, playing of individual songs, and even (perhaps in the future?) a unified search option!
 
 ## Plans
 
@@ -11,3 +11,7 @@ UniPlay, simply put, is a hub that unifies your music streaming services. Manage
   - Soundcloud (? API keys are hard to get)
 - Work on misc. features (e.g. shuffling, song discovery, searches, etc)
 - Finish and touch up appearances
+
+---
+
+Some features may not be availible in time simply due to difficulty - the YouTube API is severely impacted by embed restrictions from major publishers such as UMG. As well, the SoundCloud API keys are notoriously hard to get, since their official API key service has been shut down. Finally, the Spotify portion should be fairly easy - the only problem being that playing songs on the device requires a Premium subscription! Overall, implementation of services will be rather excruciating, and may happen over the span of a month or more.   
--- player/player.js
+++ player/player.js
@@ -1,12 +1,13 @@
 var player;
-function onYouTubeIframeAPIReady() {
+console.log('YT API loaded:', typeof YT, 'YT.Player exists?', !!YT.Player);
+function newIframe(id) {
   const target = document.getElementById('player');
   if (!target) {
     console.error('Player element not found');
     return;
   }
   player = new YT.Player('player', {
-    videoId: 'M7lc1UVf-VE',
+    videoId: id,
     events: {
       onReady: onPlayerReady,
       onError: onPlayerError
@@ -14,8 +15,44 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
+function onPlayerReady(event) {
+  console.log("Player is ready");
+}
+
 function onPlayerError(event) {
   console.error('YouTube Player Error:', event.data);
 }
 
 console.log("finished player.js script")
+
+$("#play").click(function () { 
+    if (!player) {
+        console.error("YouTube player is not ready yet.");
+        return;
+    }
+
+    const playerstate = player.getPlayerState();
+    console.log("playerstate", playerstate);
+
+    if (playerstate === 1) {
+        player.pauseVideo();
+    } else if (playerstate === 2 || playerstate === 5) {
+        player.playVideo();
+    }
+}); 
+
+$("#testvid1").click(function (e) { 
+  if (!player) {
+    newIframe("QY8dhl1EQfI");
+  } else {
+    player.cueVideoById("QY8dhl1EQfI",0);
+  }
+});
+
+$("#testvid2").click(function (e) { 
+  if (!player) {
+    newIframe("DgFSkstbBvU");
+  } else {
+    player.cueVideoById("DgFSkstbBvU",0);
+  }
+});
