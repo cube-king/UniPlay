66a451cf56aa7ca65e1285517d7cbcc5d2cd47dd
cube-king <jacksonthomaspitts@gmail.com>
2025-06-27 12:23:22 -0400

    implementing api, testing using google snippets

--- player/player.js
+++ player/player.js
@@ -12,33 +12,62 @@ function newIframe(id) {
       onReady: onPlayerReady,
       onError: onPlayerError
     }
-  });
+  }); 
 }
 
 function onPlayerReady(event) {
   console.log("Player is ready");
 }
-
+ 
 function onPlayerError(event) {
   console.error('YouTube Player Error:', event.data);
-}
+} 
 
-console.log("finished player.js script")
+function authenticate() {
+  return gapi.auth2.getAuthInstance()
+      .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+      .then(function() { console.log("Sign-in successful"); },
+            function(err) { console.error("Error signing in", err); });
+}
+function loadClient() { 
+  gapi.client.setApiKey("AIzaSyAVSegQGcXQmzzbNv-3p5xFfxlps3Powv0");
+  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+      .then(function() { console.log("GAPI client loaded for API"); },
+            function(err) { console.error("Error loading GAPI client for API", err); });
+}
+function execute() {
+    return gapi.client.youtube.playlists.list({
+      "part": [
+        "id"
+      ],
+      "maxResults": 25,
+      "mine": true 
+    })
+        .then(function(response) {    
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              }, 
+              function(err) { console.error("Execute error", err); });
+  }   
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "990409317415-474a7ac8a4jrrkr6ebj88crtbnpkbf49.apps.googleusercontent.com"});
+  });
+ 
 
 $("#play").click(function () { 
-    if (!player) {
-        console.error("YouTube player is not ready yet.");
-        return;
-    }
+  if (!player) {  
+      console.error("YouTube player is not ready yet.");
+      return; 
+  } 
 
-    const playerstate = player.getPlayerState();
-    console.log("playerstate", playerstate);
+  const playerstate = player.getPlayerState();  
+  console.log("playerstate", playerstate);
 
-    if (playerstate === 1) {
-        player.pauseVideo();
-    } else if (playerstate === 2 || playerstate === 5) {
-        player.playVideo();
-    }
+  if (playerstate === 1) {
+      player.pauseVideo();
+  } else if (playerstate === 2 || playerstate === 5) {
+      player.playVideo();
+  }
 }); 
 
 $("#testvid1").click(function (e) { 
