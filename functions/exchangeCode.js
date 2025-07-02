7a021f8a89d90c61285b74805c34e4157b71118a
cube-king <jacksonthomaspitts@gmail.com>
2025-07-02 18:00:44 -0400

    testing headers again in refactored format

--- functions/exchangeCode.js
+++ functions/exchangeCode.js
@@ -1,7 +1,29 @@
 const fetch = require("node-fetch");
 
 exports.handler = async (event) => {
-  const { code, code_verifier } = JSON.parse(event.body);
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: ""
+    };
+  }
+
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Invalid JSON in request body" })
+    };
+  }
+
+  const { code, code_verifier } = body;
 
   const client_id = process.env.CLIENT_ID;
   const redirect_uri = process.env.REDIRECT_URI;
@@ -16,17 +38,21 @@ exports.handler = async (event) => {
 
   const res = await fetch("https://oauth2.googleapis.com/token", {
     method: "POST",
-    headers: { 
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, OPTION", 
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
     },
     body: params.toString()
   });
 
   const data = await res.json();
+
   return {
     statusCode: res.ok ? 200 : 400,
+    headers: {
+      "Access-Control-Allow-Origin": "*", 
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
+    },
     body: JSON.stringify(data)
   };
 };
