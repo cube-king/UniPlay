29cfa3217cf2aa90f424d17d27bd2fb02cee68eb
cube-king <jacksonthomaspitts@gmail.com>
2025-07-02 17:03:19 -0400

    lots of npm changes and netlify changes

--- functions/exchangeCode.js
+++ functions/exchangeCode.js
@@ -1,23 +1,28 @@
 const fetch = require("node-fetch");
 
 exports.handler = async (event) => {
-  if (event.httpMethod !== "POST") return { statusCode: 405 };
+  const { code, code_verifier } = JSON.parse(event.body);
 
-  const { code } = JSON.parse(event.body);
-  if (!code) return { statusCode: 400, body: JSON.stringify({ error: "Missing code" }) };
+  const client_id = process.env.CLIENT_ID;
+  const redirect_uri = process.env.REDIRECT_URI;
 
   const params = new URLSearchParams({
     code,
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    redirect_uri: process.env.REDIRECT_URI,
+    client_id,
+    code_verifier,
+    redirect_uri,
     grant_type: "authorization_code"
   });
 
   const res = await fetch("https://oauth2.googleapis.com/token", {
-    method: "POST", headers: { "Content-Type": "application/x-www-form-urlencoded" }, body: params
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params.toString()
   });
 
   const data = await res.json();
-  return { statusCode: res.ok ? 200 : res.status, body: JSON.stringify(data) };
+  return {
+    statusCode: res.ok ? 200 : 400,
+    body: JSON.stringify(data)
+  };
 };
