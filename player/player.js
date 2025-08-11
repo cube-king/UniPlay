0dbc0ec6b576c1b402cb82cda8f29079e38a0692
cube-king <jacksonthomaspitts@gmail.com>
2025-08-11 15:45:19 -0400

    removed player.js PKCE stuff so i can start oncemore later on

--- player/player.js
+++ player/player.js
@@ -15,69 +15,6 @@ function newIframe(id) {
   }); 
 }
 
-function generateRandomString(length) {
-  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
-  let result = '';
-  const values = new Uint8Array(length);
-  window.crypto.getRandomValues(values);
-  for (let i = 0; i < values.length; i++) {
-    result += charset[values[i] % charset.length];
-  }
-  return result;
-}
-
-async function sha256(plain) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(plain);
-  const hash = await crypto.subtle.digest('SHA-256', data);
-  return new Uint8Array(hash);
-}
-
-function base64urlencode(arrayBuffer) {
-  return btoa(String.fromCharCode(...arrayBuffer))
-    .replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
-}
-
-async function createCodeChallenge(code_verifier) {
-  const hash = await sha256(code_verifier);
-  return base64urlencode(hash);
-}
-
-async function startPKCEOAuth() {
-  const clientId = "990409317415-21b44obshqu6jusjsvtf7dubbhsf8o5n.apps.googleusercontent.com";
-  const redirectUri = "https://cube-king.github.io/UniPlay/oauth2callback.html";
-  const scope = "https://www.googleapis.com/auth/youtube.readonly";
-
-  const codeVerifier = generateRandomString(128);
-  const codeChallenge = await createCodeChallenge(codeVerifier);
-  localStorage.setItem("code_verifier", codeVerifier);  
-
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code` +
-    `&client_id=${clientId}` +
-    `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-    `&scope=${encodeURIComponent(scope)}` +
-    `&code_challenge=${encodeURIComponent(codeChallenge)}` +
-    `&code_challenge_method=S256`;
-
-  window.location.href = authUrl;
-}
-
-function initGapiWithToken() {
-  const token = localStorage.getItem("access_token");
-  if (!token) {
-    console.log("Not signed in.");
-    return;
-  }
-
-  gapi.load("client", () => {
-    gapi.client.setToken({ access_token: token });
-
-    gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-      .then(() => console.log("YouTube API ready"))
-      .catch(err => console.error("API load error", err));
-  });
-}
-
 function onPlayerReady(event) {
   console.log("Player is ready");
 }
@@ -85,12 +22,6 @@ function onPlayerReady(event) {
 function onPlayerError(event) {
   console.error('YouTube Player Error:', event.data);
 } 
- 
-async function setAccessToken(token) {
-  await gapi.load('client', () => {
-    gapi.client.setToken({ access_token: token });
-  });
-}
 
 $("#play").click(function () { 
   if (!player) {  
